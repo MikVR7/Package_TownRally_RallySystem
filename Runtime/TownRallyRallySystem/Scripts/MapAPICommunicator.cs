651820b2f156865d3e8a70dde48280f366645d7b
MikVR7 <atmikes123@gmail.com>
2022-06-06 21:57:52 +0200

    update map system

--- Runtime/TownRallyRallySystem/Scripts/MapAPICommunicator.cs
+++ Runtime/TownRallyRallySystem/Scripts/MapAPICommunicator.cs
@@ -1,3 +1,5 @@
+using CoDeEvents;
+using Sirenix.OdinInspector;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,22 +9,41 @@ using UnityEngine;
 
 namespace TownRally.RallySystem
 {
-    internal class MapAPICommunicator : MonoBehaviour
+    internal class EventIn_ReportTag : EventSystem<string, string> { }
+    internal class MapAPICommunicator : SerializedMonoBehaviour
     {
+        internal static EventIn_ReportTag EventIn_ReportTag = new EventIn_ReportTag();
+
         [SerializeField] private Vector2 bottomLeft = Vector2.zero;
         [SerializeField] private Vector2 topRight = Vector2.zero;
         [SerializeField] private GameObject prefabNode = null;
         [SerializeField] private Transform tMap = null;
         [SerializeField] private GameObject prefabWay = null;
+        [SerializeField] private string pathToMapData = string.Empty;
 
         private Dictionary<string, OnlineMapsOSMNode> nodes;
         private List<OnlineMapsOSMWay> ways;
         private List<OnlineMapsOSMRelation> relations;
         private List<OnlineMapsOSMArea> areas;
+        [SerializeField] private Dictionary<string, int> tagKeys = new Dictionary<string, int>();
 
         private void Start()
         {
-            string requestData = String.Format("node({0},{1},{2},{3});way(bn)[{4}];area[de: regionalschluessel~ ^ 057](._;>;);out;",
+            EventIn_ReportTag.AddListener(ReportTag);
+        }
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.H))
+            {
+                string data = File.ReadAllText(pathToMapData);
+                DisplayData(data);
+            }
+        }
+
+        private void LoadMapDataOnline() {
+            // area[de: regionalschluessel~ ^ 057];
+            string requestData = String.Format("node({0},{1},{2},{3});way(bn)[{4}];(._;>;);out;",
                 bottomLeft.x.ToString(OnlineMapsUtils.numberFormat),
                 bottomLeft.y.ToString(OnlineMapsUtils.numberFormat),
                 topRight.x.ToString(OnlineMapsUtils.numberFormat),
@@ -39,15 +60,14 @@ namespace TownRally.RallySystem
         /// This event called when the request is completed.
         private void OnComplete(string response)
         {
-            
-
-            //// Get nodes, ways and relations from response
-            OnlineMapsOSMAPIQuery.ParseOSMResponse(response, out nodes, out ways, out relations, out areas);
-
-            Debug.Log("NODES: " + nodes.Count + " Ways: " + ways.Count + " Relations: " + relations.Count + " AREA: " + areas.Count);
-
             SaveFile(response);
+            DisplayData(response);
+        }
 
+        private void DisplayData(string data) {
+            //// Get nodes, ways and relations from response
+            OnlineMapsOSMAPIQuery.ParseOSMResponse(data, out nodes, out ways, out relations, out areas);
+            Debug.Log("NODES: " + nodes.Count + " Ways: " + ways.Count + " Relations: " + relations.Count + " AREA: " + areas.Count);
             this.VisualizeNodes();
             this.VisualizeWays();
         }
@@ -68,7 +88,9 @@ namespace TownRally.RallySystem
                 Transform tNode = goNode.GetComponent<Transform>();
                 tNode.localPosition = new Vector3((node.lat-47f)*1000f, 0.01f, (node.lon-15f)*1000f);
                 tNode.localEulerAngles = new Vector3(90f, 0f, 0f);
-                tNode.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                tNode.localScale = new Vector3(0.02f, 0.02f, 0.02f);
+                MapNode mapNode = goNode.GetComponent<MapNode>();
+                mapNode.Init(node);
             }
         }
 
@@ -83,7 +105,20 @@ namespace TownRally.RallySystem
                 lrWay.positionCount = wNodes.Count;
                 int index = 0;
                 wNodes.ForEach(wNode => { lrWay.SetPosition(index++, new Vector3((wNode.lat-47f) * 1000f, 0f, (wNode.lon-15f)*1000f)); });
+                MapWay mapWay = goWay.GetComponent<MapWay>();
+                mapWay.Init(w);
             });
+        }
+
+        private void ReportTag(string key, string value)
+        {
+            if(!this.tagKeys.ContainsKey(key)) { 
+                this.tagKeys.Add(key, 1);
+            }
+            else
+            {
+                this.tagKeys[key]++;
+            }
         }
     }
 }
